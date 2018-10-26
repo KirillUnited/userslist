91f9f926dda29084dd1dd6888897094ffd2cfe7e
k.dubov <email@example.email>
2018-10-26 17:47:24 +0300

    add routes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,19 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RootComponent } from './root/root.component';
-import { ROUTING } from './app.routing';
-import {NgxPaginationModule} from 'ngx-pagination';
+
+import { NgxPaginationModule } from 'ngx-pagination';
 import { LoginComponent } from './login/login.component';
 import { UsersService } from './users.service';
+import { SettingsComponent } from './settings/settings.component';
+import { RouterModule } from '@angular/router';
+
+const routes = [
+  { path: '', component: LoginComponent },
+  { path: 'settings', component: SettingsComponent }
+]
 
 @NgModule({
   declarations: [
     RootComponent,
     LoginComponent,
+    SettingsComponent,
   ],
   imports: [
     BrowserModule,
-    ROUTING,
+    RouterModule.forRoot(routes),
     FormsModule,
     HttpClientModule,
     NgxPaginationModule
